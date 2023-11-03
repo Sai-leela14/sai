cb4774caec89dace2d702233e8877dfe7abf068f
Sai-leela14 <149146080+Sai-leela14@users.noreply.github.com>
2023-11-03 12:39:39 +0530

    Add files via upload

---
+++ trim.java
@@ -0,0 +1,9 @@
+public class trim
+{  
+public static void main(String ar[])  
+{  
+String s="  Sachin kumar  ";    
+System.out.println(s);//  Sachin      
+System.out.println(s.trim());//Sachin    
+}  
+}  
